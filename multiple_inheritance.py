08e82c83f7933c17694c68736bae16d6e81992a2
akash <akashvpm440@gmail.com>
2023-12-31 12:11:15 +0530

    multiple_inheritance

--- multiple_inheritance.py
+++ multiple_inheritance.py
@@ -1,23 +1,22 @@
-class human:
+class Human:
       def eat (self):
             print("I can eat vegitables, meals ")
       def work (self):
             print(" I can perform all kind of works")
 
-class men(human):
+class Men:
       def __init__(self,name):
             self.name = name 
             print(f"hi i am {self.name}")
       def work (self):
-            super().work()
             print("Especially i can code well")
-class women(human):
+            
+class Women(Human,Men):
       pass
 
-men_1 = men("akash")
-women_1 = women()
+men_1 = Men("akash")
+women_1 = Women("hela")
 
-men_1.work()
-men_1.eat()
+
 women_1.work()
 
