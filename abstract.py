2bafff280f4758d19ba30cdc929054ff7944905e
akash <akashvpm440@gmail.com>
2024-01-12 01:40:32 +0530

    abstract

---
+++ abstract.py
@@ -0,0 +1,9 @@
+from abc import ABC,abstractmethod
+def vechile(ABC):
+      def __init__ (self,n):
+            self.no_of_wheels = n
+      @abstractmethod
+      def start():
+            pass
+      def display():
+            print("I call from the vechicle")
